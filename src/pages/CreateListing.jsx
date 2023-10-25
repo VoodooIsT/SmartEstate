b616f14f73c931f57a73efba7b1249a904747395
VoodooIsT <abshukla773p@gmail.com>
2023-10-25 23:47:59 +0530

    added image upload functionaloty to upload it

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,61 @@
+import { useState } from "react"
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
 
 
 const CreateListing = () => {
+
+
+  const [file, setFile] = useState([]);
+  const[formData, setFormData] = useState({
+    imageUrls:  [],
+
+  })
+
+  console.log(formData);
+
+  const handleImageSubit = (e) => {
+    if(file.length > 0 && file.length < 7) {
+      const promises = [];
+      
+      for(let i = 0; i < file.length; i++) {
+        promises.push(storeImage(file[i]));
+
+      }
+      Promise.all(promises).then((urls) => {
+        setFormData({...formData, imageUrls: formData.imageUrls.concat(urls)})
+      })
+    }
+
+  };
+
+  const storeImage = async(file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`)
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+            resolve(downloadURL)
+          );
+        }
+      );
+    })
+  }
+
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
         <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
@@ -72,8 +127,8 @@ const CreateListing = () => {
           </p>
 
           <div className="flex gap-4">
-            <input type="file" id="images" accept="image/*" multiple  className="p-3 border border-gray-300 rounded w-full"/>
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">Upload</button>
+            <input onChange={(e)=>setFile(e.target.files)} type="file" id="images" accept="image/*" multiple  className="p-3 border border-gray-300 rounded w-full"/>
+            <button type="button" onClick={handleImageSubit} className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">Upload</button>
           </div>
           <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Create Listing</button>
         </div>
